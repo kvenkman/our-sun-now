dd1a340df16b95fe817b767b1fb665a8bc07eab0
kvenkman <karthikvt@vt.edu>
2023-02-20 23:27:26 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
     
     <div className="App-header">
-        <div className="App-text">Our Sun right now</div>
+        <div className="App-text">Our Sun now</div>
     </div>
     
     <AIAButtonGroup />
+    <br/ >
     <p className="App-text"> This data is from the <a href="https://sdo.gsfc.nasa.gov/"> <code> Solar Dynamics Observatory </code> </a> </p>
     </div>
   )
